e7f1eb4d16c2a8632d451240ab6d8c4ad9953bed
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-11-21 17:15:41 -0300

    rename

--- back-end/src/controllers/DowloadController.ts
+++ back-end/src/controllers/DowloadController.ts
@@ -2,9 +2,7 @@ import { Application, Request, Response } from 'express';
 import { DIR_VIEWS } from '../ts-env';
 
 export default class DowloadController {
-  public index(req: Request, res: Response): void {
-    res.sendFile(DIR_VIEWS + '/dowload/formDowload.html');
-  }
+  public index(req: Request, res: Response): void {}
 
   public listMusics(req: Request, res: Response): void {}
 }
